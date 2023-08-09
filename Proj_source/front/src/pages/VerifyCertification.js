7bfa0136c23e1a467dce3a40e2b0836af77d8d4b
Stefano <stefano.faccio@studenti.unitn.it>
2023-08-09 15:30:58 +0200

    foo

--- Proj_source/front/src/pages/VerifyCertification.js
+++ Proj_source/front/src/pages/VerifyCertification.js
@@ -1,12 +1,174 @@
 import React from 'react';
 import MainLayout from '../layout/MainLayout';
+import { useForm, SubmitHandler } from "react-hook-form";
+import ElectricEngine from './../artifacts/ElectricEngine.json' //import project contract
+import { DrizzleContext } from '@drizzle/react-plugin';
+import { Drizzle } from "@drizzle/store";
+import { newContextComponents } from "@drizzle/react-components";
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useDrizzle, useDrizzleState } from '@drizzle/react-plugin';
+import { DrizzleProvider } from '@drizzle/react-plugin';
+import { DrizzleContextProvider } from '@drizzle/react-plugin';
+import { DrizzleContextConsumer } from '@drizzle/react-plugin';
+import './../index.css';
+
 
 function VerifyCertification() {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => console.log(data)
+
+    console.log(watch("example"))
+
     return (
         <MainLayout>
             <h1>VerifyCertification</h1>
+            <DrizzleContext.Provider drizzle={drizzle}>
+                <form>
+                    <input id="lot" type="text" name="lott" placeholder="lott" ></input>
+                    <button type="button" onClick={lotCert}>Submit</button>
+                </form>
+                <p id="risp"></p>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {/* register your input into the hook by invoking the "register" function */}
+                    <input defaultValue="test" {...register("example")} />
+
+                    {/* include validation with required or other standard HTML validation rules */}
+                    <input {...register("exampleRequired", { required: true })} />
+                    {/* errors will return when field validation fails  */}
+                    {errors.exampleRequired && <span>This field is required</span>}
+
+                    <input type="submit" />
+                </form>
+
+                <main className="  h-screen items-center flex justify-center">
+        <form
+          className="bg-white flex rounded-lg w-1/2 font-latoRegular"
+        >
+          <div className="flex-1 text-gray-700  p-20">
+            <h1 className="text-3xl pb-2 font-latoBold">
+              Let's get started 👋
+            </h1>
+            <p className="text-lg  text-gray-500">
+              Join our E-learning platform today and unlock over 500+ courses
+              and digital assets ready to download.
+            </p>
+            <div className="mt-6 ">
+              {/* Name input field */}
+              <div className="pb-4">
+                <label
+                  htmlFor="name"
+                  className={`block font-latoBold text-sm pb-2`}
+                >
+                </label>
+                <p className="text-sm font-latoBold text-red-400 "></p>
+                <input
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500 "
+                  type="text"
+                  name="name"
+                  placeholder="Enter your name"
+                />
+              </div>
+              {/* Email input field */}
+              <div className="pb-4">
+                <label
+                  htmlFor="email"
+                  className={`block font-latoBold text-sm pb-2 `}
+                >
+                </label>
+
+                <p></p>
+                <input
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500"
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email address"
+                />
+              </div>
+              {/* Country input field */}
+              <div className="pb-4">
+                <label
+                  htmlFor="country"
+                  className="block font-latoBold text-sm pb-2"
+                >
+                  Country
+                </label>
+                <select
+                  className="border-2 border-gray-500 p-2 rounded-md w-1/2 focus:border-teal-500 focus:ring-teal-500"
+                  name="country"
+                >
+                  <option>United States</option>
+                  <option>United Kingdom</option>
+                  <option>Germany</option>
+                </select>
+              </div>
+              {/* Terms of service*/}
+              <div className="pb-4">
+                <label
+                  htmlFor="terms"
+                  className={`block font-latoBold text-sm pb-2`}
+                >
+                </label>
+
+                <div className="flex items-center gap-2">
+                  <input
+                    type="checkbox"
+                    name="terms"
+                    value="checked"
+                    className="h-5 w-5 text-teal-500 border-2  background-gray-500 focus:border-teal-500 focus:ring-teal-500"
+                  />
+                  <p className="text-sm font-latoBold text-gray-500">
+                    I agree to the Terms and Service that my data will be taken
+                    and sold.
+                  </p>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="bg-teal-500 font-latoBold text-sm text-white py-3 mt-6 rounded-lg w-full"
+              >
+                Start learning today!
+              </button>
+            </div>
+          </div>
+          <div className="relative flex-1">
+          </div>
+        </form>
+      </main>
+            </DrizzleContext.Provider>
         </MainLayout>
     );
 }
 
 export default VerifyCertification;
+
+//Set contract in drizzle option
+const drizzleOptions =
+{
+    contracts: [ElectricEngine],
+};
+
+const { AccountData, ContractData, ContractForm } = newContextComponents;
+
+const drizzle = new Drizzle(drizzleOptions);
+
+//Function to ask if the engines lot is certified
+function lotCert() {
+    var state = drizzle.store.getState();
+    var lotto = document.getElementById('lot').value;
+
+    // If Drizzle is initialized (and therefore web3, accounts and contracts), continue.
+    if (state.drizzleStatus.initialized) {
+
+        //Call contract's method
+        const dataKey = drizzle.contracts.ElectricEngine.methods.isCertificatedEngines(lotto).call();
+        //Visualize the answer
+        dataKey.then(value => { document.getElementById('risp').innerHTML = value; })
+    }
+}
