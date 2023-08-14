02c7588d892983360660c6f534cc2d5a91fb99b1
RiccardoNicolin <72378380+RiccardoNicolin@users.noreply.github.com>
2023-08-14 15:32:47 +0200

    Home.js - added presentatoin of the project

--- Proj_source/front/src/pages/Home.js
+++ Proj_source/front/src/pages/Home.js
@@ -10,6 +10,11 @@ function Home() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
+    <p>
+    This project was born to provide a secure way to certify the process of manufacturing of multiple products to make sure that they comply with
+    the European regulation and operate inside their optimal parameter. It also provides a way to track all the steps in the production, 
+    from the prime materials to the final product and all eventual intermediate phases.
+    </p>
     </div>
   );
 }
